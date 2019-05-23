7eaa657c2f175c09cbb894bd0f03174e9db13d8f
Sefik Ilkin Serengil <serengil@gmail.com>
2019-05-23 09:14:38 +0300

    initialize params

--- commons/functions.py
+++ commons/functions.py
@@ -29,3 +29,45 @@ def storeRule(file,content):
 def createFile(file,content):
 	f = open(file, "w")
 	f.write(content)
+
+def initializeParams(config):
+	algorithm = 'ID3'
+	enableRandomForest = False; num_of_trees = 5; enableMultitasking = False
+	enableGBM = False; epochs = 10; learning_rate = 1
+	enableAdaboost = False
+	debug = False
+	
+	for key, value in config.items():
+		if key == 'debug':
+			debug = value
+		elif key == 'algorithm':
+			algorithm = value
+		#---------------------------------	
+		elif key == 'enableRandomForest':
+			enableRandomForest = value
+		elif key == 'num_of_trees':
+			num_of_trees = value
+		elif key == 'enableMultitasking':
+			enableMultitasking = value
+		#---------------------------------
+		elif key == 'enableGBM':
+			enableGBM = value
+		elif key == 'epochs':
+			epochs = value
+		elif key == 'learning_rate':
+			learning_rate = value
+		#---------------------------------	
+		elif key == 'enableAdaboost':
+			enableAdaboost = value
+			
+	config['debug'] = debug
+	config['algorithm'] = algorithm
+	config['enableRandomForest'] = enableRandomForest
+	config['num_of_trees'] = num_of_trees
+	config['enableMultitasking'] = enableMultitasking
+	config['enableGBM'] = enableGBM
+	config['epochs'] = epochs
+	config['learning_rate'] = learning_rate
+	config['enableAdaboost'] = enableAdaboost
+	
+	return config
