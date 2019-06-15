eca2175c285636b508b70194286ff3a26b604760
Şefik Serangil <sefik.serengil@softtech.com.tr>
2019-06-15 13:23:47 +0300

    output folder removed

--- outputs/rules/rules-for-Iris-setosa-round-1.py
+++
@@ -1,5 +0,0 @@
-def findDecision(obj): #obj[0]: Sepal length, obj[1]: Sepal width, obj[2]: Petal length, obj[3]: Petal width
-   if obj[2]>1.9:
-      return -0.3333333432674408
-   elif obj[2]<=1.9:
-      return 0.6666666567325592
