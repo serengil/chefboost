341c78f176cf8ee6aadec2df15db62fbd066d597
Sefik Ilkin Serengil <serengil@gmail.com>
2019-06-18 17:50:28 +0300

    new return types

--- Dispatcher.py
+++ Dispatcher.py
@@ -2,36 +2,26 @@ import Chefboost as chef
 import pandas as pd
 import numpy as np
 
-mae = 0
-
 #------------------------------------
 
 #gradient boosting regression tree
 config = {'enableGBM': True, 'epochs': 7, 'learning_rate': 1}
 
 df = pd.read_csv("dataset/golf4.txt")
-models = chef.fit(df.copy(), config)
-
-for index, instance in df.iterrows():
-	actual = instance['Decision']
-	prediction = chef.predict(config, models, instance.values)
-	
-	error = abs(actual - prediction)
-	mae += error
-	print("Prediction: ",prediction,", Actual: ",actual)
-
-print("MAE: ",mae/df.shape[0])
+model = chef.fit(df.copy(), config)
+prediction = chef.predict(model, ['Sunny',85,85,'Weak'])
+print("Prediction: ",prediction)
 
 #------------------------------------
 
 #gradient boosting classification
 config = {'algorithm': 'ID3', 'enableGBM': True, 'epochs': 5, 'learning_rate': 1}
 
-models, classes = chef.fit(pd.read_csv("dataset/iris.data", names=["Sepal length","Sepal width","Petal length","Petal width","Decision"]), config)
+model = chef.fit(pd.read_csv("dataset/iris.data", names=["Sepal length","Sepal width","Petal length","Petal width","Decision"]), config)
 
 test_set = [7.0,3.2,4.7,1.4]
 
-prediction = chef.predict(config, models, test_set, classes)
+prediction = chef.predict(model, test_set)
 print(prediction)
 
 #------------------------------------
@@ -40,7 +30,17 @@ print(prediction)
 config = {'algorithm': 'C4.5'}
 model = chef.fit(pd.read_csv("dataset/golf2.txt"), config)
 
-prediction = chef.predict(config, model, ['Sunny',85,85,'Weak'])
+prediction = chef.predict(model, ['Sunny',85,85,'Weak'])
+#prediction = model[0].findDecision(['Sunny',85,85,'Weak'])
+print(prediction)
+
+#------------------------------------
+
+#regular decision tree
+config = {'algorithm': 'CART'}
+model = chef.fit(pd.read_csv("dataset/golf.txt"), config)
+
+prediction = chef.predict(model, ['Sunny','Hot','High','Weak'])
 print(prediction)
 
 #------------------------------------
@@ -48,7 +48,7 @@ print(prediction)
 #regular decision tree
 config = {'algorithm': 'ID3'}
 model = chef.fit(pd.read_csv("dataset/golf.txt"), config)
-prediction = chef.predict(config, model, ['Sunny', 'Hot', 'High', 'Weak'])
+prediction = chef.predict(model, ['Sunny', 'Hot', 'High', 'Weak'])
 print(prediction)
 
 #------------------------------------
@@ -57,27 +57,27 @@ print(prediction)
 config = {'algorithm': 'Regression'}
 model = chef.fit(pd.read_csv("dataset/golf3.txt"), config)
 
-prediction = chef.predict(config, model, ['Sunny', 'Hot', 'High', 'Weak'])
+prediction = chef.predict(model, ['Sunny', 'Hot', 'High', 'Weak'])
 print(prediction)
 
 #---------------------------------
 
 #adaboost
 config = {'enableAdaboost': True, 'num_of_weak_classifier': 4}
-models, alphas = chef.fit(pd.read_csv("dataset/adaboost.txt"), config)
-prediction = chef.predict(config, models, [4, 3.5], alphas)
+model = chef.fit(pd.read_csv("dataset/adaboost.txt"), config)
+prediction = chef.predict(model, [4, 3.5])
 print(prediction)
 
 #------------------------------------
 
 #random forest - classification
 config = {'algorithm': 'ID3', 'enableRandomForest': True, 'num_of_trees': 5,}
-models = chef.fit(pd.read_csv("dataset/car.data"), config)
+model = chef.fit(pd.read_csv("dataset/car.data"), config)
 
-prediction = chef.predict(config, models, ['vhigh','vhigh',2,'2','small','low'])
+prediction = chef.predict(model, ['vhigh','vhigh',2,'2','small','low'])
 print(prediction)
 
-prediction = chef.predict(config, models, ['high','high',4,'more','big','high'])
+prediction = chef.predict(model, ['high','high',4,'more','big','high'])
 print(prediction)
 
 #------------------------------------
