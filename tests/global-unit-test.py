337a98d9aee441eb0203596065999513d1dd0aad
Sefik Ilkin Serengil <serengil@gmail.com>
2020-01-30 23:09:03 +0300

    parelellism

--- tests/global-unit-test.py
+++ tests/global-unit-test.py
@@ -4,94 +4,99 @@ import sys
 
 from chefboost import Chefboost as cb
 
-print("ID3 for nominal features and target:")
-config = {'algorithm': 'ID3'}
-cb.fit(pd.read_csv("dataset/golf.txt"), config)
+#----------------------------------------------
+parallelism_cases = [False, True]
 
-print("-------------------------")
+for enableParallelism in parallelism_cases:
 
-print("ID3 for nominal/numeric features and target:")
-config = {'algorithm': 'ID3'}
-cb.fit(pd.read_csv("dataset/golf2.txt"), config)
+  print("ID3 for nominal features and target:")
+  config = {'algorithm': 'ID3', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("C4.5 for nominal/numeric features and target:")
-config = {'algorithm': 'C4.5'}
-cb.fit(pd.read_csv("dataset/golf2.txt"), config)
+  print("ID3 for nominal/numeric features and target:")
+  config = {'algorithm': 'ID3', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf2.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("CART for nominal/numeric features and target:")
-config = {'algorithm': 'CART'}
-cb.fit(pd.read_csv("dataset/golf2.txt"), config)
+  print("C4.5 for nominal/numeric features and target:")
+  config = {'algorithm': 'C4.5', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf2.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("regression tree for nominal features, numeric target")
-config = {'algorithm': 'Regression'}
-cb.fit(pd.read_csv("dataset/golf3.txt"), config)
+  print("CART for nominal/numeric features and target:")
+  config = {'algorithm': 'CART', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf2.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("regression tree for nominal/numeric features, numeric target")
-config = {'algorithm': 'Regression'}
-cb.fit(pd.read_csv("dataset/golf4.txt"), config)
+  print("regression tree for nominal features, numeric target")
+  config = {'algorithm': 'Regression', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf3.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("algorithm must be regression tree for numetic target. set any other algorithm.")
-config = {'algorithm': 'ID3'}
-cb.fit(pd.read_csv("dataset/golf4.txt"), config)
+  print("regression tree for nominal/numeric features, numeric target")
+  config = {'algorithm': 'Regression', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf4.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("ID3 for nominal features and target (large data set)")
-config = {'algorithm': 'ID3'}
-cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
+  print("algorithm must be regression tree for numetic target. set any other algorithm.")
+  config = {'algorithm': 'ID3', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf4.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("C4.5 for nominal features and target (large data set)")
-config = {'algorithm': 'C4.5'}
-cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
+  print("ID3 for nominal features and target (large data set)")
+  config = {'algorithm': 'ID3', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("CART for nominal features and target (large data set)")
-config = {'algorithm': 'CART'}
-cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
+  print("C4.5 for nominal features and target (large data set)")
+  config = {'algorithm': 'C4.5', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("Adaboost")
-config = {'algorithm': 'ID3', 'enableAdaboost': True}
-cb.fit(pd.read_csv("dataset/adaboost.txt"), config)
+  print("CART for nominal features and target (large data set)")
+  config = {'algorithm': 'CART', 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("Regular GBM")
-config = {'algorithm': 'CART', 'enableGBM': True, 'epochs': 10, 'learning_rate': 1}
-cb.fit(pd.read_csv("dataset/golf4.txt"), config)
+  print("Adaboost")
+  config = {'algorithm': 'ID3', 'enableAdaboost': True, 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/adaboost.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("GBM for classification")
-config = {'algorithm': 'ID3', 'enableGBM': True, 'epochs': 10, 'learning_rate': 1}
-cb.fit(pd.read_csv("dataset/iris.data", names=["Sepal length", "Sepal width", "Petal length", "Petal width", "Decision"]), config)
+  print("Regular GBM")
+  config = {'algorithm': 'CART', 'enableGBM': True, 'epochs': 10, 'learning_rate': 1, 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/golf4.txt"), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-print("Random forest")
-config = {'algorithm': 'ID3', 'enableRandomForest': True, 'num_of_trees': 5, 'enableMultitasking': False}
-cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
+  print("GBM for classification")
+  config = {'algorithm': 'ID3', 'enableGBM': True, 'epochs': 10, 'learning_rate': 1, 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/iris.data", names=["Sepal length", "Sepal width", "Petal length", "Petal width", "Decision"]), config)
 
-print("-------------------------")
+  print("-------------------------")
 
-"""
-print("Random forest (multitasking)")
-config = {'algorithm': 'ID3', 'enableGBM': False, 'epochs': 10, 'learning_rate': 1, 'enableRandomForest': True, 'num_of_trees': 5, 'enableMultitasking': True, 'enableAdaboost': False}
-cb.fit(pd.read_csv("dataset/car.data",names=["buying","maint","doors","persons","lug_boot","safety","Decision"]), config)
-"""
-print("-------------------------")
-print("unit tests completed successfully...")
+  print("Random forest")
+  config = {'algorithm': 'ID3', 'enableRandomForest': True, 'num_of_trees': 5, 'enableMultitasking': False, 'enableParallelism': enableParallelism}
+  cb.fit(pd.read_csv("dataset/car.data", names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "Decision"]), config)
+
+  print("-------------------------")
+
+  """
+  print("Random forest (multitasking)")
+  config = {'algorithm': 'ID3', 'enableGBM': False, 'epochs': 10, 'learning_rate': 1, 'enableRandomForest': True, 'num_of_trees': 5, 'enableMultitasking': True, 'enableAdaboost': False}
+  cb.fit(pd.read_csv("dataset/car.data",names=["buying","maint","doors","persons","lug_boot","safety","Decision"]), config)
+  """
+  print("-------------------------")
+  print("unit tests completed successfully...")
