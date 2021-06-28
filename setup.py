61803801cf5fd6a512843aa5797f91366901a6be
Sefik Ilkin Serengil <serengil@gmail.com>
2021-06-28 22:24:08 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="chefboost",
-    version="0.0.13",
+    version="0.0.14",
     author="Sefik Ilkin Serengil",
     author_email="serengil@gmail.com",
     description="Lightweight Decision Tree Framework Supporting GBM, Random Forest and Adaboost",
