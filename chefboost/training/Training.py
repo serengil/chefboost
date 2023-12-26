9bf190d1649c18f6514e810ce74fa6f7e159018f
Sefik Ilkin Serengil <serengil@gmail.com>
2023-12-26 14:47:20 +0000

    get rid of print

--- chefboost/training/Training.py
+++ chefboost/training/Training.py
@@ -559,7 +559,6 @@ def buildDecisionTree(
     if df.Decision.dtypes == "object":  # classification
         pivot = pd.DataFrame(df.Decision.value_counts()).sort_values(by=["count"], ascending=False)
         else_decision = f"return '{str(pivot.iloc[0].name)}'"
-        print(f"else decision is {else_decision}")
 
         if enableParallelism != True:
             functions.storeRule(file, (functions.formatRule(root), "else:"))
